06dba2bd92d5226c07cff93d4af78763af8636b2
snaushadalam <snaushadalam@radial.com>
2020-11-11 00:50:53 +0530

    user-model-association except rails migration

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -22,6 +22,7 @@ class ArticlesController < ApplicationController
 
   def create
     @article = Article.new(article_params)
+    @article.user=User.first
     if @article.save
       flash[:notice]=" successfully saved!!!"
       redirect_to @article
