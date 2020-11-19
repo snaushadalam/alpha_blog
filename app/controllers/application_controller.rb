9622b63e99ee03886393a04dd71078342185cc0c
snaushadalam <snaushadalam@radial.com>
2020-11-19 21:03:03 +0530

    Restriction through URL and also deleting profile

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,16 @@
 class ApplicationController < ActionController::Base
-  helper_method :current_user,:logged_in?
+  helper_method :current_user,:logged_in?,:require_user
   def current_user
     @current_user ||=User.find(session[:user_id]) if(session[:user_id])
   end
   def logged_in?
     !!current_user
+  end
+
+  def require_user
+    if !logged_in?
+    flash[:alert] = "you need to login"
+    redirect_to login_path
+    end
   end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,7 @@
 class UsersController <ApplicationController
-  before_action :set_user ,only: [:show ,:edit ,:update]
+  before_action :set_user ,only: [:show ,:edit ,:update,:destroy]
+  before_action :require_user, only: [:edit,:update,]
+  before_action :require_same_user, only: [:edit,:update,:destroy]
   def show
     @articles = @user.articles.paginate(page: params[:page], per_page: 2)
   end
@@ -31,6 +33,12 @@ class UsersController <ApplicationController
       render 'new'
     end
   end
+  def destroy
+    @user.destroy
+    session[:user_id]=nil
+    flash[:alert]="User successfully Deleted"
+    redirect_to root_path
+  end
 
   private
   def user_params
@@ -39,4 +47,10 @@ class UsersController <ApplicationController
 def set_user
   @user = User.find(params[:id])
 end
+  def require_same_user
+    if current_user!=@user
+      flash[:alert]="you must edit only your Profile"
+      redirect_to @user
+    end
+  end
 end
